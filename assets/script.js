7a9826bb9da8e5d2299d742fec3241f881b68ea7
DKsolomon <DKSolomon8135@gmail.com>
2023-02-02 19:27:08 -0800

    added event listener for inputs

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,14 @@
-var today = dayjs().format("dddd, MMMM D YYYY, h:mm:ss a")
-$("#currentDay").text(today);
+var today = dayjs();
 
-$(function () {
- 
+$("#currentDay").text(today.format("dddd, MMMM D YYYY, h:mm:ss a"));
+
+$(".btn").on("click", function (event) {
+  event.preventDefault();
+  var inputValue = $(this).siblings(".description").val();
+  var hour = $(this).parent().attr("id");
+  localStorage.setItem(hour, inputValue);
+});
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -21,4 +27,4 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+
