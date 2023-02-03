6c58814f37ac93aa4002642d2612f38a124b0fca
DKsolomon <DKSolomon8135@gmail.com>
2023-02-02 20:07:18 -0800

    added past, present, and future color funtion

--- assets/script.js
+++ assets/script.js
@@ -9,6 +9,43 @@ $(".btn").on("click", function (event) {
   localStorage.setItem(hour, inputValue);
 });
 
+$(".time-block").each(function () {
+
+  var pageTime = $(this).attr("id").split("-")[1];
+  var currentTime = dayjs().hour();
+
+  if (pageTime === currentTime) {
+    $(this).removeClass("past");
+    $(this).removeClass("future");
+    $(this).addClass("present");
+
+  } else if (pageTime > currentTime) {
+    $(this).removeClass("present");
+    $(this).removeClass("past");
+    $(this).addClass("future");
+
+  } else if (pageTime < currentTime) {
+    $(this).removeClass("future");
+    $(this).removeClass("present");
+    $(this).addClass("past");
+
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+    $("#hour-18 .description").val(localStorage.getItem("hour-18"));
+    $("#hour-19 .description").val(localStorage.getItem("hour-19"));
+    $("#hour-20 .description").val(localStorage.getItem("hour-20"));
+    $("#hour-21 .description").val(localStorage.getItem("hour-21"));
+
+  
+}});
+ 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
